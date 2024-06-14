6a0606662c973cf60bcb52c7f592a0a3162338c7
daminoworld <ekadls757@gmail.com>
2024-06-15 02:14:15 +0900

    CreateTestView file

---
+++ 13jo/13jo/TestCard/TestCardView.swift
@@ -0,0 +1,18 @@
+//
+//  TestCardView.swift
+//  13jo
+//
+//  Created by Damin on 6/15/24.
+//
+
+import SwiftUI
+
+struct TestCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestCardView()
+}
