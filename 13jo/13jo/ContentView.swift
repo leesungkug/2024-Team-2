aa8b8fe91129b24e93bd71986e32f59e5b58900b
sungkug_apple_developer_ac <sunglee@Kkugy-MackBook-Pro.local>
2024-06-14 23:21:14 +0900

    기본 프로젝트 생성

---
+++ 13jo/13jo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  13jo
+//
+//  Created by sungkug_apple_developer_ac on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 13jo/13jo/_3joApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3joApp.swift
+//  13jo
+//
+//  Created by sungkug_apple_developer_ac on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct _3joApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
